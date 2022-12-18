93c6a5386ba9a6adeb805c3a5b5d69f07425d18f
Aadityaa23 <aadityaa32@gmail.com>
2022-12-18 15:03:41 +1100

    Added Navbar to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,46 @@
+import { TabPanelUnstyled } from "@mui/base";
+import {
+	AppBar,
+	Menu,
+	MenuItem,
+	Container,
+	Toolbar,
+	Typography,
+	Button,
+	Grid,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
 function Home() {
-	return <h1>Home Page</h1>;
+	return (
+		<React.Fragment>
+			<AppBar position="static">
+				<Toolbar
+					sx={{
+						justifyContent: "space-between",
+						height: 75,
+					}}
+				>
+					<Box>
+						<Typography variant="h2">Carbon Coin</Typography>
+					</Box>
+
+					<Box>
+						<Button>
+							<Typography Container="h3">Home</Typography>{" "}
+						</Button>
+						<Button>
+							<Typography Container="h3">About Us</Typography>{" "}
+						</Button>
+						<Button>
+							<Typography Container="h3">Sign Up</Typography>{" "}
+						</Button>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</React.Fragment>
+	);
 }
 
 export default Home;
